661d7ac43b758d6be6499be36f8fadb3c022a31f
Ben Bangert <ben@groovie.org>
2013-12-26 21:43:53 -0800

    updates

--- src/Megahaskhal.hs
+++ src/Megahaskhal.hs
@@ -2,7 +2,15 @@ module Megahaskhal (
     loadBrainFromFilename
     ) where
 
-import System.Ramdon (getStdRandom, randomR)
+import Data.Word
+import System.Random (getStdRandom, randomR)
 import Megahaskhal.Serialization (loadBrainFromFilename)
 import qualified Megahaskhal.Internal as I
 
+seed :: Tree -> IO Word16
+seed t =
+    | childLength == 0 = return 0
+    | otherwise = do
+        let childIndex = getStdRandom (randomR (1, childLength))
+        return $ I.getSymbol $ I.getChildren t !! childIndex
+    where childLength = length $ I.getChildren t
