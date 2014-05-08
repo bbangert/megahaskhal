dc2d1e6e2e7afae27c0d7dc1cdaefa69d201aab9
Ben Bangert <ben@groovie.org>
2014-05-07 19:41:15 -0700

    Add brain serialization.

--- src/Megahaskhal/Serialization.hs
+++ src/Megahaskhal/Serialization.hs
@@ -5,23 +5,28 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Megahaskhal.Serialization (
-  -- * Deserialization
-    loadBrainFromFilename
+  -- * Serialization
+    saveBrainWithFilename
 
+  -- * Deserialization
+  , loadBrainFromFilename
   ) where
 
 import           Control.Applicative      ((<$>), (<*>))
 import qualified Data.Binary.Get          as G
+import qualified Data.Binary.Put          as P
+import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy     as BL
 import qualified Data.Text                as T
 import qualified Data.Text.Encoding       as T
 import qualified Data.Text.Encoding.Error as T
 import qualified Data.Vector              as V
-import           System.IO                (IOMode (ReadMode), withFile)
+import           System.IO                (IOMode (ReadMode, WriteMode),
+                                           withBinaryFile, withFile)
 
 import qualified Megahaskhal.Dictionary   as D
 import           Megahaskhal.Internal     (Brain (Brain))
-import           Megahaskhal.Tree         (Tree, getChildren, mkTree)
+import           Megahaskhal.Tree         (Tree (Tree), getChildren, mkTree)
 
 w8, w16, w32 :: G.Get Int
 w8 = fromIntegral <$> G.getWord8
@@ -63,3 +68,31 @@ parseTree = (return $!) =<<
 parseText :: Int -> G.Get T.Text
 parseText n = (return $!) =<<
   T.decodeUtf8With T.lenientDecode <$> G.getByteString (fromIntegral n)
+
+saveBrainWithFilename :: Brain -> String -> IO ()
+saveBrainWithFilename brain fileName = withBinaryFile fileName WriteMode saveFile
+  where saveFile handle = BL.hPut handle $ P.runPut $ saveBrain brain
+
+saveBrain :: Brain -> P.Put
+saveBrain (Brain ft bt cookie order dict) = do
+  P.putByteString "MegaHALv8"
+  P.putWord8 $ fromIntegral order
+  putTree ft
+  putTree bt
+  P.putWord32le $ fromIntegral $ D.length dict
+  D.forM_ dict putText
+
+putTree :: Tree -> P.Put
+putTree (Tree symbol usage count children) = do
+  P.putWord16le symbol
+  P.putWord32le usage
+  P.putWord16le count
+  P.putWord16le $ fromIntegral $ V.length children
+  V.forM_ children putTree
+
+putText :: T.Text -> P.Put
+putText txt =  do
+  let wd = T.encodeUtf8 txt
+      len = B.length wd
+  P.putWord8 $ fromIntegral len
+  P.putByteString wd
